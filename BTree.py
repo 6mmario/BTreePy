1009387eb333bb1a536b53259af79439ff8a3f93
robert <toby_77@users.sourceforge.net>
2014-07-08 22:57:16 +0300

    - improved add algorithm and made it more clear

--- BTree.py
+++ BTree.py
@@ -55,9 +55,21 @@ class BTreeNode:
     """
     def _add(self, tree, val, slot=None, childNodes=None):
         # all insertions should start at a leaf node,
-        # unless we call add as a result of node spliting
+        # unless we call _add recursively into the parrent
+        # as a result of node spliting
         # when we are adding the median value to the parent
         assert(self.children is None or childNodes)
+
+        # if this is an inner node if not a leaf or the root
+        # then self.children is not None, then also
+        # this function must have been called recursively
+        # with childNodes not None, val not None and 
+        # len(childNodes) == 2
+        innerNode = self.children is not None
+        if innerNode:
+            assert(childNodes and len(childNodes) == 2)
+        else:
+            assert(childNodes is None)
         
         if slot is None:
             slot = bisect.bisect_left(self.values, val)
@@ -70,43 +82,71 @@ class BTreeNode:
                 for i in childNodes:
                     i.parent = self
                 self.children[slot:slot + 1] = childNodes
+            # we're done
+            return True
+       
+        # get the median of self.values and val
+        medianIdx = len(self.values) // 2
+
+        # lists of new child nodes split by the median value that 
+        # we are to determine
+        if innerNode:
+            lc = []
+            rc = []
         else:
-            # get the median of val and slots
-            vl = self.values[:]
-            vl.insert(slot, val)
-            if len(vl) % 2 == 1:
-                medianIdx = (len(vl) // 2)
-            else:
-                medianIdx = (len(vl) // 2) - 1
-            median = vl[medianIdx]
-            cl = None
-            if childNodes:
-                cl = self.children[:]
-                cl[slot:slot + 1] = childNodes
+            lc = None
+            rc = None
+        
+        if slot < medianIdx:
+            lv = self.values[0:slot]
+            lv.extend([ val ])
+            lv.extend(self.values[slot:medianIdx])
+            medianVal = self.values[medianIdx]
+            rv = self.values[medianIdx + 1:]
+            if innerNode:
+                lc = self.children[0:slot]
+                lc.extend(childNodes)
+                lc.extend(self.children[slot + 1:medianIdx])
+                rc.extend(self.children[medianIdx:])
+        elif slot == medianIdx:
+            lv = self.values[0:medianIdx]
+            medianVal = val
+            rv = self.values[medianIdx:]
+            if innerNode:
+                lc.extend(self.children[0:medianIdx])
+                lc.extend([ childNodes[0] ])
+                rc.extend([ childNodes[1] ])
+                rc.extend(self.children[medianIdx + 1:])
+        else:
+            # slot > medianIdx
+            lv = self.values[0:medianIdx]
+            medianVal = self.values[medianIdx]
+            rv = self.values[medianIdx + 1:slot]
+            rv.extend([ val ])
+            rv.extend(self.values[slot:])
+            if innerNode:
+                lc.extend(self.children[0:medianIdx + 1])
+                rc.extend(self.children[medianIdx + 1:slot])
+                rc.extend(childNodes)
+                rc.extend(self.children[slot + 1:])
 
-            # construct new left node
-            vl1 = vl[0:medianIdx]
-            cl1 = None
-            if cl:
-                cl1 = cl[0:medianIdx + 1]
-            leftNode = BTreeNode(vl1, cl1)
-            # construct the right node
-            vl2 = vl[medianIdx + 1:]
-            cl2 = None
-            if cl:
-                cl2 = cl[medianIdx + 1:]
-            rightNode = BTreeNode(vl2, cl2)
+        leftNode = BTreeNode(lv, lc)
+        rightNode = BTreeNode(rv, rc)
 
-            if self.parent:
-                self.parent._add(tree, median, None, (leftNode, rightNode))
-            else:
-                # create new root and increment the tree depth
-                newParent = BTreeNode([ median ], [leftNode, rightNode])
-                leftNode.parent = newParent
-                rightNode.parent = newParent
-                tree.root = newParent
-                tree.height += 1
-                tree.size += 1
+        if self.parent:
+            return self.parent._add(tree,
+                                    medianVal,
+                                    None,
+                                    (leftNode, rightNode))
+
+        # create new root and increment the tree depth
+        newRoot = BTreeNode([ medianVal ], [leftNode, rightNode])
+        leftNode.parent = newRoot
+        rightNode.parent = newRoot
+        tree.root = newRoot
+        tree.height += 1
+        tree.size += 1
+        return True
 
 
 class BTree:
@@ -138,13 +178,13 @@ def btree_test():
     tree = BTree(4)
     # for i in range(15, 0, -1):
     # for i in [5, 3, 21, 9, 1, 13, 2, 7, 10, 12, 4, 8]:
-    for i in range(1, 16):
+    for i in range(1, 10):
         print("insert: %s" % i)
         tree.add(i)
         print('%r\n%r' % (tree, tree.root))
     print("-------")
     tree.search(77799)
-    for i in range(1, 16):
+    for i in range(1, 10):
         if not tree.search(i):
             raise
     print("Done.")
